312e03fec06ed01d9cf37137b497b4730de368c7
mhmjhamyargroup44233 <mhmjhamyar@gmail.com>
2016-02-28 14:18:32 +0330

    Update invite.lua

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -78,7 +78,7 @@ return {
 
 end
 
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
+--Copyright; @Mhmafi
+--Persian Translate; @Mhmafi
+--ch : @HamayarGroupp
+--همیار گروه
