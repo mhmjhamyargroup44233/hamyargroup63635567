c0fbf9c2d98176d8f1d654f3656285e3d99cd638
mhmjhamyargroup44233 <mhmjhamyar@gmail.com>
2016-02-28 13:35:32 +0330

    Update lock_join.lua

--- plugins/lock_join.lua
+++ plugins/lock_join.lua
@@ -17,7 +17,7 @@ return {
   },
   run = run
 }
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
+--Copyright and edit; @Mhmafi
+--Persian Translate; @Mhmafi
+--ch : @HamyarGroupp
+--همیار گروه
