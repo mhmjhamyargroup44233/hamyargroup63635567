1ee6b60ded48ddfdea691e66e3dec3afd7da5bee
mhmjhamyargroup44233 <mhmjhamyar@gmail.com>
2016-02-29 14:33:16 +0330

    Update anti_spam.lua

--- plugins/anti_spam.lua
+++ plugins/anti_spam.lua
@@ -129,7 +129,7 @@ return {
 }
 
 end
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
+--Copyright; @Mhmafi
+--Persian Translate; @Mhmafi
+--ch : @HmayarGroupp
+--همیار گروه
