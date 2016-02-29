04e12c8601dc89710cde2b1ecb0de8d3733cf0a2
mhmjhamyargroup44233 <mhmjhamyar@gmail.com>
2016-02-29 13:52:15 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -7,10 +7,10 @@ do
  
    local text = matches[1]
    local chat = "chat#id"..158990680 
-   --like : local chat = "chat#id"..12345678
+   --like : local chat = "chat#id"..30373376
    
   local sends = send_msg(chat, fuse, ok_cb, false)
-  return 'بازخورد شما ارسال شد'
+  return '✅پیام شما با موفقیت به پشتیبانی ارسال شد
  
  end
  end
@@ -25,7 +25,7 @@ do
   },
   run = run
  }
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
+--Copyright; @Mhmafi
+--Persian Translate; @Mhmafi
+--ch : @HamyarGroupp
+--همیار گروه
