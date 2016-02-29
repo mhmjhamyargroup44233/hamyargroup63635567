509bf8a7ce38ec3d7c3af27d0e8e5695fcbdf00d
mhmjhamyargroup44233 <mhmjhamyar@gmail.com>
2016-02-29 13:43:53 +0330

    Update google.lua

--- plugins/google.lua
+++ plugins/google.lua
@@ -39,7 +39,7 @@ return {
   },
   run = run
 }
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
+--Copyright and edit; @Mhmafi
+--Persian Translate; @Mhmafi
+--ch : @HamyarGroupp
+--همیار گروه
