10fc60241989fff9f8c47feb1efe736402c014b7
mhmjhamyargroup44233 <mhmjhamyar@gmail.com>
2016-02-29 14:16:09 +0330

    Update boobs.lua

--- plugins/boobs.lua
+++ plugins/boobs.lua
@@ -70,7 +70,7 @@ return {
 }
 
 end
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
+--Copyright; @Mhmafi
+--Persian Translate; @Mhmafi
+--ch : @HamyarGroupp
+--همیار گروه
